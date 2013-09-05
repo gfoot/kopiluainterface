9fab3448932380666b4900efc17da2acc9ba759c
George Foot <george.foot@gmail.com>
2013-09-06 00:53:54 +0100

    publish script creates ver files and includes copyrights in the target folde (cherry picked from commit f80f07dbbc5c7646763591ebb144c3e282ce6e90)

--- publish.sh
+++ publish.sh
@@ -11,7 +11,7 @@ if [ -z "$type" ]; then
     type="Release"
 fi
 modules="KopiLua KopiLuaDll KopiLuaInterface"
-files=""
+files=$root/KLI-bin-skel/*
 for module in $modules; do
     files="$files $root/$module/bin/$type/$module.dll"
     files="$files $root/$module/bin/$type/$module.pdb"
@@ -20,6 +20,23 @@ done
 echo "Copying these files to $target_dir:"
 ls -l $files
 cp -f $files $target_dir
+
+echo "Writing ver files..."
+write_ver()
+{
+	path="$1"
+	repo=$2
+	sha=$3
+	cat > "$path" <<EOF
+repo: $repo
+sha: $sha
+url: https://github.com/$repo/commit/$sha
+EOF
+}
+
+write_ver $target_dir/KopiLua.ver gfoot/kopilua `git --git-dir=$root/KopiLua/.git log -1 --pretty=format:%H`
+write_ver $target_dir/KopiLuaDll.ver gfoot/kopiluainterface `git --git-dir=$root/.git log -1 --pretty=format:%H`
+write_ver $target_dir/KopiLuaInterface.ver gfoot/kopiluainterface `git --git-dir=$root/.git log -1 --pretty=format:%H`
 
 echo "Generating MDB files..."
 unityeditorexe=`cat "/proc/registry/HKEY_CURRENT_USER/Software/Unity Technologies/Unity Editor 3.x/Location/@"`
