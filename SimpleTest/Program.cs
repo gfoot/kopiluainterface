e65b07af1ad4601a59dbce405f7f6b61566ba171
George Foot <george.foot@gmail.com>
2013-09-20 19:42:17 +0100

    Add test for 'out' and 'ref' handling in luanet.get_method_bysig

--- SimpleTest/Program.cs
+++ SimpleTest/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 using LuaInterface;
 
 namespace SimpleTest
 {
-    class Foo
+    public class Foo
     {
         public double m = 3;
 
@@ -14,9 +13,15 @@ namespace SimpleTest
         {
             return x * m;
         }
+
+        public static bool OutMethod(Foo foo, out Bar bar)
+        {
+            bar = new Bar() { x = foo.m };
+            return true;
+        }
     };
 
-    class Bar
+    public class Bar
     {
         public double x;
     };
@@ -53,6 +58,13 @@ namespace SimpleTest
             lua["x"] = 3;
             lua.DoString("y=x");
             Console.WriteLine("y={0}", lua["y"]);
+
+            {
+                lua.DoString("luanet.load_assembly('SimpleTest')");
+                lua.DoString("Foo = luanet.import_type('SimpleTest.Foo')");
+                lua.DoString("method = luanet.get_method_bysig(Foo, 'OutMethod', 'SimpleTest.Foo', 'out SimpleTest.Bar')");
+                Console.WriteLine(lua["method"]);
+            }
 
             {
                 object[] retVals = lua.DoString("return 1,'hello'");
